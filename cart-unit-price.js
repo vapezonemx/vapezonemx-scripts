6b9c4d6cae3954c9aa8b7ac3857662839c1b32c9
vapezonemx <vapezonemty.contacto@gmail.com>
2026-09-11 13:00:19 -0600

    Update cart-unit-price.js

--- cart-unit-price.js
+++ cart-unit-price.js
@@ -8,29 +8,13 @@
   // 1) TABLA DE PRECIOS POR MODELO
   // ---------------------------------------------------------------
 
-  // 🔀 INTERRUPTOR: true = catálogo con descuento vigente (15%/10% off)
-  //                 false = catálogo a precio completo
-  const DISCOUNT_ACTIVE = true;
-
-  const DISCOUNT_VARIANTS = {
-    "NOVABAR 35K": {
-      active:   [[1,9,340],[10,49,170],[50,149,165],[150,300,161]],
-      inactive: [[1,9,400],[10,49,200],[50,149,195],[150,300,190]],
-    },
-    "NEXA PIX": {
-      active:   [[1,9,340],[10,49,195],[50,149,191],[150,300,187]],
-      inactive: [[1,9,400],[10,49,230],[50,149,225],[150,300,220]],
-    },
-    "FASTA 35000": {
-      active:   [[1,9,315],[10,49,207],[50,149,202],[150,300,198]],
-      inactive: [[1,9,350],[10,49,230],[50,149,225],[150,300,220]],
-    },
-  };
-
+  // Actualizado: Lista de Precios VapeZone 2026 V61.1
   const PRICE_TABLE = {
-    "NOVABAR 35K": DISCOUNT_ACTIVE ? DISCOUNT_VARIANTS["NOVABAR 35K"].active : DISCOUNT_VARIANTS["NOVABAR 35K"].inactive,
-    "NEXA PIX": DISCOUNT_ACTIVE ? DISCOUNT_VARIANTS["NEXA PIX"].active : DISCOUNT_VARIANTS["NEXA PIX"].inactive,
-    "FASTA 35000": DISCOUNT_ACTIVE ? DISCOUNT_VARIANTS["FASTA 35000"].active : DISCOUNT_VARIANTS["FASTA 35000"].inactive,
+    // NOTA: NOVABAR, NEXA PIX y FASTA ya no usan descuento activo/inactivo,
+    // el catálogo 2026 trae su precio definitivo directo.
+    "NOVABAR 35K": [[1,9,400],[10,300,200]],
+    "NEXA PIX": [[1,9,400],[10,49,230],[50,149,225],[150,300,220]],
+    "FASTA 35000": [[1,9,400],[10,300,300]],
 
     // ---- IPLAY ----
     "IPLAY PRO MAX": [[1,9,400],[10,49,245],[50,149,240],[150,300,235]],
@@ -50,9 +34,9 @@
 
     // ---- Otras marcas ----
     "BILLIONAIRE BOYS": [[1,9,200],[10,null,150]],
-    "VHILL 3000": [[1,9,240],[10,49,140],[50,149,135],[150,300,130]],
+    "VHILL 3000": [[1,9,250]],
     "VHILL 6000": [[1,9,300],[10,49,200],[50,149,195],[150,300,190]],
-    "VHILL 12000": [[1,9,400],[10,10,245]],
+    "VHILL 12000": [[1,9,400]],
     "VHILL 32000": [[1,9,480]],
 
     "GEEK BAR PULSE X": [[1,9,350],[10,49,240],[50,149,235],[150,300,230]],
@@ -60,10 +44,15 @@
     "X RETURNS": [[1,9,350],[10,49,240],[50,149,235],[150,300,230]],
 
     "TOMORO D20": [[1,9,400],[10,49,240],[50,149,235],[150,300,230]],
+    "TOMORO MAX": [[1,9,345],[10,49,210],[50,149,205],[150,300,200]],
+    "SUONON DONETE": [[1,9,400],[10,49,270],[50,149,265],[150,300,260]],
     "INSTABAR MEGA 80K": [[1,9,450],[10,49,270],[50,149,265],[150,300,260]],
+    "INSTABAR 15K": [[1,9,300],[10,300,195]],
+    "INSTABAR 70K": [[1,9,400],[10,49,260],[50,149,255],[150,300,250]],
     "VOOM METEOR 70K": [[1,9,400],[10,49,270],[50,149,265],[150,300,260]],
+    "VPLAY 20K": [[1,9,300],[10,300,225]],
 
-    "VFLY ZERO CLOUD": [[1,9,400],[10,49,240],[50,149,235],[150,300,230]],
+    "VFLY ZERO CLOUD": [[1,9,320],[10,49,192],[50,149,188],[150,300,184]],
     "VFLY": [[1,9,200],[10,null,150]],
 
     "WONDER G8": [[1,9,300],[10,1000,160]],
@@ -83,7 +72,7 @@
     "IJOY CAPTAIN": [[1,9,150],[10,300,150]],
     "HEYLO": [[1,9,145],[10,300,145]],
     "SKWEZED": [[1,9,145],[10,300,145]],
-    "LANAVAPE AIRSHIP": [[1,9,150],[10,300,150]],
+    "LANAVAPE AIRSHIP": [[1,9,300],[10,300,180]],
     "PACKSPOD": [[1,9,145],[10,300,145]],
     "POD MESH": [[1,9,145],[10,300,145]],
     "SPACE MAX": [[1,9,145],[10,300,145]],
